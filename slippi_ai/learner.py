217cdb4f5c60e407f19154267b1c7ca1c7a82c86
stephenwu <stephw42@uw.edu>
2023-02-13 23:13:52 -0500

    added print statement

--- slippi_ai/learner.py
+++ slippi_ai/learner.py
@@ -175,21 +175,23 @@ class OfflineVTraceLearner:
           bootstrap_value=bootstrap_value,
       )
 
-      total_loss = compute_policy_gradient_loss(
-          target_logprobs,
-          vtrace_returns.pg_advantages)
-      policy_gradient_loss = compute_policy_gradient_loss(
-          target_logprobs,
-          vtrace_returns.pg_advantages)
+      total_loss = compute_baseline_loss(vtrace_returns.vs-values)
+      # total_loss = compute_policy_gradient_loss(
+      #     target_logprobs,
+      #     vtrace_returns.pg_advantages)
+      # policy_gradient_loss = compute_policy_gradient_loss(
+      #     target_logprobs,
+      #     vtrace_returns.pg_advantages)
 
-      value_loss = compute_baseline_loss(vtrace_returns.vs-values)
-      value_stddev = tf.sqrt(tf.reduce_mean(value_loss))
-      total_loss += self.value_cost * value_loss
+      # value_loss = compute_baseline_loss(vtrace_returns.vs-values)
+      # value_stddev = tf.sqrt(tf.reduce_mean(value_loss))
+      # total_loss += self.value_cost * value_loss
 
-      teacher_loss = -compute_entropy_loss(behavior_logprobs)
-      total_loss += self.teacher_cost * teacher_loss
+      # teacher_loss = -compute_entropy_loss(behavior_logprobs)
+      # total_loss += self.teacher_cost * teacher_loss
 
       behavior_loss = -behavior_logprobs
+      tf.print(total_loss,output_stream=sys.stdout)
       if self.train_behavior_policy:
         print('training which should be wrong')
         total_loss += behavior_loss
